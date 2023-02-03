b3e88445e6a9b49f231e2b9dc766f9352b87797b
Jordan Neville <jordan.neville@odaia.ai>
2023-02-03 09:44:20 -0500

    More formatting tweaks

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ module.exports = function () {
 
             var openTag = `<testcase classname="${this.currentFixture.name.replace("Feature: ", "")}" ` +
                           `file="${this.currentFixture.path}" ` +
-                          `time="${testRunInfo.durationMs / 1000}">\n`;
+                          `name=" " time="${testRunInfo.durationMs / 1000}">\n`;
 
             this.report += this.indentString(openTag, 2);
 
